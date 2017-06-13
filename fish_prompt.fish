91d74ddd5272aba05455b6fb1aeb9362d22f3cb7
Radek SPRTA <mail@radeksprta.eu>
2017-06-13 16:57:34 +0200

    Clean up code

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -7,46 +7,13 @@ function fish_prompt --description 'Write out the prompt'
 
 	set -l normal (set_color normal)
 
-	# Hack; fish_config only copies the fish_prompt function (see #736)
-	if not set -q -g __fish_classic_git_functions_defined
-		set -g __fish_classic_git_functions_defined
-
-		function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-		
-		function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-		
-		function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		# initialize our new variables
-		if not set -q __fish_improved_git_prompt_initialized
-			set -qU fish_color_user; or set -U fish_color_user -o magenta
-			set -qU fish_color_local; or set -U fish_color_local -o green
-			set -qU fish_color_remote; or set -U fish_color_remote -o red
-			set -qU fish_color_status; or set -U fish_color_status -o yellow
-			set -U __fish_improved_git_prompt_initialized
-		end
-	end
-
+    # initialize our new variables
+    set -U fish_color_user -o magenta
+    set -U fish_color_local -o green
+    set -U fish_color_remote -o red
+    set -U fish_color_status -o yellow
 	set -l color_cwd
-	set -l prefix
+
 	switch $USER
 	case root toor
 		if set -q fish_color_cwd_root
