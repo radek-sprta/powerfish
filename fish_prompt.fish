f2cb557482fe808c7b620979d92e7ff530e94fd7
Radek SPRTA <mail@radeksprta.eu>
2017-06-13 15:59:20 +0200

    Change colors

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -37,11 +37,11 @@ function fish_prompt --description 'Write out the prompt'
 		end
 
 		# initialize our new variables
-		if not set -q __fish_classic_git_prompt_initialized
-			set -qU fish_color_user; or set -U fish_color_user -o green
-			set -qU fish_color_host; or set -U fish_color_host -o pink
-			set -qU fish_color_status; or set -U fish_color_status red
-			set -U __fish_classic_git_prompt_initialized
+		if not set -q __fish_improved_git_prompt_initialized
+			set -qU fish_color_user; or set -U fish_color_user -o yellow
+			set -qU fish_color_host; or set -U fish_color_host -o green
+			set -qU fish_color_status; or set -U fish_color_status magenta
+			set -U __fish_improved_git_prompt_initialized
 		end
 	end
 
@@ -65,5 +65,5 @@ function fish_prompt --description 'Write out the prompt'
 		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
 	end
 
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status "> "
+	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) (set_color $fish_color_status) (__fish_vcs_prompt) $normal "> "
 end
