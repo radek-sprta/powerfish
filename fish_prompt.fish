ba45b02854253a0450dc4f228341dc9e91cae2f1
Radek SPRTA <mail@radeksprta.eu>
2017-06-14 12:30:52 +0200

    Change to Poweline-like look

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,30 +1,31 @@
 set -U git_status_clean yellow
-set -U git_status_dirty -o red
+set -U git_status_dirty red
 
 function git_branch_status -d 'See if there is anything to commit'
-	if test (git status 2> /dev/null | tail -n1) != 'nothing to commit, working tree clean'
-   		echo (set_color $git_status_dirty)
+	if test (git status 2> /dev/null ^&1 | tail -n1) != 'nothing to commit, working tree clean'
+   		echo $git_status_dirty
 	else
-   		echo (set_color $git_status_clean)
+   		echo $git_status_clean
 	end
 end
 
 function fish_prompt --description 'Write out the prompt'
 
     # Initialize colors
-    set -U fish_color_user -o magenta
-    set -U fish_color_local -o green
-    set -U fish_color_remote -o red
-    set -U fish_color_status -o yellow
+    set -U fish_color_user 444
+    set -U fish_color_local green
+    set -U fish_color_remote red
+    set -U fish_color_status yellow
+	set -U fish_color_cwd (set_color black -b blue)
 	set -l color_cwd
-	set -l normal (set_color normal)
+	set -l normal (set_color white)
 
 	# Just calculate this once, to save a few cycles when displaying the prompt
 	if not set -q __fish_prompt_hostname
 		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
 	end
 	if not set -q __fish_prompt_git_branch 
-		set __fish_prompt_git_branch (git_branch_status)"("(git branch ^/dev/null | grep \* | sed 's/* //')")"$normal
+		set __fish_prompt_git_branch $normal(git branch ^/dev/null | grep \* | sed 's/* //')
 	end
 
 	switch $USER
@@ -46,16 +47,20 @@ function fish_prompt --description 'Write out the prompt'
         if set -q fish_color_local
             set fish_color_host $fish_color_local
         else
-            set fish_color_host -o green
+            set fish_color_host green
         end
     case '*'
         if set -q fish_color_remote
             set fish_color_host $fish_color_remote
         else
-            set fish_color_host -o red
+            set fish_color_host red
         end
     end
             
 
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) "$__fish_prompt_git_branch" $normal "> "
+	echo -n -s $normal (set_color -b $fish_color_user) "$USER"\
+(set_color $fish_color_user -b $fish_color_host) $normal (set_color -b $fish_color_host) "$__fish_prompt_hostname"\
+(set_color $fish_color_host -b blue)  $color_cwd (prompt_pwd)\
+(set_color blue -b (git_branch_status))  "$__fish_prompt_git_branch"\
+(set_color (git_branch_status) -b normal)' ' $normal
 end
