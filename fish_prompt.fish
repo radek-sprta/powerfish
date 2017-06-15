5b171384e6a6dde482041ab22cf9c39180e9fa38
Radek SPRTA <mail@radeksprta.eu>
2017-06-15 11:25:10 +0200

    Various graphical improvements

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,15 +1,17 @@
 # Characters
 set -g SEPARATOR ''
 
-set -U git_status_clean yellow
-set -U git_status_dirty red
 
 function git_branch_status -d 'See if there is anything to commit'
 	if test (git status 2> /dev/null ^&1 | tail -n1) != 'nothing to commit, working tree clean'
-   		echo $git_status_dirty
+   		echo 'dirty'
 	else
-   		echo $git_status_clean
+   		echo 'clean'
 	end
+end
+
+function git_branch_name -d 'Get current git branch name'
+    echo (git branch ^/dev/null | grep \* | sed 's/* //')
 end
 
 function __prompt_segment -d 'Draw prompt segment' 
@@ -46,6 +48,8 @@ function fish_prompt --description 'Write out the prompt'
     set -U fish_color_bg_normal 444
     set -U fish_text_light white
     set -U fish_text_dark black
+    set -U git_status_clean yellow
+    set -U git_status_dirty red
     set -U fish_color_user $fish_color_bg_normal
     set -U fish_color_root red
     set -U fish_color_local $fish_color_bg_normal
@@ -58,9 +62,6 @@ function fish_prompt --description 'Write out the prompt'
 	# Just calculate this once, to save a few cycles when displaying the prompt
 	if not set -q __fish_prompt_hostname
 		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
-	if not set -q __fish_prompt_git_branch 
-		set __fish_prompt_git_branch $normal(git branch ^/dev/null | grep \* | sed 's/* //')
 	end
     # Use different colors for local and remote hosts
     if not set -q __fish_prompt_host_color
@@ -78,15 +79,26 @@ function fish_prompt --description 'Write out the prompt'
     case normal
         set fish_color_user $fish_color_bg_normal
     end
+    # Git branch prompt segment
+	if not set -q __fish_prompt_git_branch 
+        if test -n (git_branch_name)
+            switch (git_branch_status)
+            case dirty
+                set __fish_prompt_git_branch (__prompt_segment $fish_text_light $git_status_dirty)\
+                (git_branch_name)(__prompt_segment $git_status_dirty normal)
+            case clean
+                set __fish_prompt_git_branch (__prompt_segment $fish_text_dark $git_status_clean)\
+                (git_branch_name)(__prompt_segment $git_status_clean normal)
+            end
+        else
+            set __fish_prompt_git_branch (__prompt_segment $fish_color_cwd normal)
+        end
+	end
 
-    # TODO Nice boundary between user and local
     # TODO Add status
-    # TODO Switch font color as well, not just background
-	echo -n -s (set -g current_background $fish_color_bg_normal) (set_color $fish_text_light -b $fish_color_user) "$USER"\
-        " at"\
+	echo -n -s (set -g current_background $fish_color_user) (set_color $fish_text_light -b $fish_color_user) "$USER"\
         (__prompt_segment $fish_text_light $__fish_prompt_host_color)"$__fish_prompt_hostname"\
         (__prompt_segment $fish_text_dark $fish_color_cwd) (prompt_pwd)\
-        (__prompt_segment $fish_text_dark (git_branch_status)) "$__fish_prompt_git_branch"\
-        (__prompt_segment (git_branch_status) normal)
-        set_color normal
+         "$__fish_prompt_git_branch"\
+        (set_color normal)" "
 end
