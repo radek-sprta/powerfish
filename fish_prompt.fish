a5181e9b4c1b92cf05647a7dc288b8004e00e000
Radek SPRTA <mail@radeksprta.eu>
2017-06-16 02:39:53 +0200

    Show more detailed git info

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -2,19 +2,7 @@
 set -g SEPARATOR ''
 set -g OK '✔'
 set -g FAILED '✘'
-
-
-function git_branch_status -d 'See if there is anything to commit'
-	if test (git status 2> /dev/null ^&1 | tail -n1) != 'nothing to commit, working tree clean'
-   		echo 'dirty'
-	else
-   		echo 'clean'
-	end
-end
-
-function git_branch_name -d 'Get current git branch name'
-    echo (git branch ^/dev/null | grep \* | sed 's/* //')
-end
+set -g BRANCH ''
 
 function __prompt_segment -d 'Draw prompt segment' 
 	set -l fg $argv[1]
@@ -44,6 +32,71 @@ function __is_root -d 'Check if user is root'
 	end
 end
 
+function __git_prompt -d "Write out the git prompt"
+
+    function __is_git_dirty -d 'Check if repo is dirty'
+        echo (git status -s --ignore-submodules=dirty ^/dev/null)
+    end
+
+    function __git_branch_name -d 'Get branch name'
+        echo (git branch ^/dev/null | grep \* | sed 's/* //')
+    end
+
+    function __set_git_color -d 'Set color depending on the tree status'
+        if test -n (__is_git_dirty)
+            # Check if there are untracked files
+            set -l untracked (git ls-files --other --exclude-standard 2> /dev/null)
+            if test -n "$untracked"
+                set -g git_status_color $git_color_untracked
+                set -g git_status_text $fish_text_light
+            else
+                set -g git_status_color $git_color_dirty
+                set -g git_status_text $fish_text_dark
+            end
+        else
+            set -g git_status_color $git_color_clean
+            set -g git_status_text $fish_text_dark
+        end
+    end
+
+    function __count -d 'Count the various git statuses'
+        echo (echo $argv[1] | cut -d ' ' -f 1)
+    end
+
+    if test -n (__git_branch_name)
+        if test -n (__is_git_dirty)
+            for i in (git status --porcelain | cut -c 1-2 | sort | uniq -c | sed -e 's/^[[:space:]]*//')
+                switch (echo $i | cut -d ' ' -f 2)
+                    case "*[ahead *"
+                        set git_flags "$git_flags ⬆ "(__count $i)
+                    case "*behind *"                  
+                        set git_flags "$git_flags ⬇ "(__count $i)
+                    case "*A*"                         
+                        set git_flags "$git_flags ✚ "(__count $i)
+                    case " D"                         
+                        set git_flags "$git_flags ✖ "(__count $i)
+                    case "*M*"                        
+                        set git_flags "$git_flags ● "(__count $i)
+                    case "*R*"                        
+                        set git_flags "$git_flags ➜ "(__count $i)
+                    case "*U*"                        
+                        set git_flags "$git_flags ═ "(__count $i)
+                    case "??"                        
+                        set git_flags "$git_flags … "(__count $i)
+                end
+            end
+        else
+            set git_flags ""
+        end
+        __set_git_color
+        echo -n -s (__prompt_segment $git_status_text $git_status_color)\
+                   " $BRANCH "(__git_branch_name)"$git_flags "\
+                   (__prompt_segment $git_status_color normal)
+    else
+        echo (__prompt_segment $fish_color_cwd normal)
+    end
+end
+
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
 
@@ -51,8 +104,9 @@ function fish_prompt --description 'Write out the prompt'
     set -U fish_color_bg_normal 444
     set -U fish_text_light white
     set -U fish_text_dark black
-    set -U git_status_clean yellow
-    set -U git_status_dirty red
+    set -U git_color_untracked red
+    set -U git_color_dirty yellow
+    set -U git_color_clean green
     set -U fish_color_user $fish_color_bg_normal
     set -U fish_color_root red
     set -U fish_color_local $fish_color_bg_normal
@@ -68,8 +122,6 @@ function fish_prompt --description 'Write out the prompt'
     if not set -q __fish_prompt_status
         if test $last_status -ne 0
             set __fish_prompt_status (set_color red -b $fish_color_user)"$FAILED "
-        else
-            set __fish_prompt_status (set_color green -b $fish_color_user)"$OK "
         end
     end
     # Hostname
@@ -92,26 +144,11 @@ function fish_prompt --description 'Write out the prompt'
     case normal
         set fish_color_user $fish_color_bg_normal
     end
-    # Git branch prompt segment
-	if not set -q __fish_prompt_git_branch 
-        if test -n (git_branch_name)
-            switch (git_branch_status)
-            case dirty
-                set __fish_prompt_git_branch (__prompt_segment $fish_text_light $git_status_dirty)\
-                (git_branch_name)(__prompt_segment $git_status_dirty normal)
-            case clean
-                set __fish_prompt_git_branch (__prompt_segment $fish_text_dark $git_status_clean)\
-                (git_branch_name)(__prompt_segment $git_status_clean normal)
-            end
-        else
-            set __fish_prompt_git_branch (__prompt_segment $fish_color_cwd normal)
-        end
-	end
 
 	echo -n -s (set -g current_background $fish_color_user) "$__fish_prompt_status"\
         (set_color $fish_text_light -b $fish_color_user) "$USER"\
         (__prompt_segment $fish_text_light $__fish_prompt_host_color)"$__fish_prompt_hostname"\
         (__prompt_segment $fish_text_dark $fish_color_cwd) (prompt_pwd)\
-         "$__fish_prompt_git_branch"\
+        (__git_prompt)\
         (set_color normal)" "
 end
