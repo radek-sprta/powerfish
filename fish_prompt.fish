0694d6af4e1764e641d53bd7269344cbc460c2d2
Radek SPRTA <mail@radeksprta.eu>
2017-06-26 23:47:47 +0200

    Fix whitespace sometime messing up hostname

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -84,7 +84,7 @@ function __hostname_prompt -d "Write out the hostname prompt"
 	end
 
     # Only show remote hosts
-    set -l ppid (ps --format ppid= --pid %self)
+    set -l ppid (ps --format ppid= --pid %self | tr -d '[:space:]')
     switch (ps --format comm= --pid $ppid)
     case sshd
         echo (__prompt_segment $fish_text_dark $fish_color_remote)" at $__fish_prompt_hostname "
